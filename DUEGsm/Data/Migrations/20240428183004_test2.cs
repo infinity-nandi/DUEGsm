1762b6a9596a6557337c7915d8e2c5ac2456f97e
Virág Nándor <nandorrexexel@gmail.com>
2024-04-28 20:32:50 +0200

    Small updates

---
+++ DUEGsm/Data/Migrations/20240428183004_test2.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DUEGsm.Data.Migrations
+{
+    public partial class test2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Mobiles_Mobiles_MobileId",
+                table: "Mobiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Mobiles_MobileId",
+                table: "Mobiles");
+
+            migrationBuilder.DropColumn(
+                name: "MobileId",
+                table: "Mobiles");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MobileId",
+                table: "Mobiles",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mobiles_MobileId",
+                table: "Mobiles",
+                column: "MobileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Mobiles_Mobiles_MobileId",
+                table: "Mobiles",
+                column: "MobileId",
+                principalTable: "Mobiles",
+                principalColumn: "Id");
+        }
+    }
+}
--- DUEGsm/Models/MobilViewModel.cs
+++ DUEGsm/Models/MobilViewModel.cs
@@ -8,7 +8,7 @@ namespace DUEGsm.Models
         public SelectList? Modell { get; set; }
         public SelectList? Brand { get; set; }
         public SelectList? Order { get; set; }
-        public string? ProductModel { get; set; }
+        public string? ProductModell { get; set; }
         public string? ProductBrand { get; set; }
         public string? SearchString { get; set; }
         public int? OrderValue { get; set; }
