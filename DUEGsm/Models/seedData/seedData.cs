11f7b4f3a4551f2abd28fed07b18c985e9c14ca1
Virág Nándor <nandorrexexel@gmail.com>
2024-05-03 01:21:04 +0200

    Frontend fejlesztések

--- DUEGsm/Models/seedData/seedData.cs
+++ DUEGsm/Models/seedData/seedData.cs
@@ -27,7 +27,7 @@ namespace DUEGsm.Models.seedData
                         FrontCamera = "14px",
                         BackCamera = "28",
                         Stroage = 124,
-                        Description = "Description",
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In mollis nunc sed id. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit lectus. Ac placerat vestibulum lectus mauris. Pulvinar mattis nunc sed blandit libero. Nec feugiat in fermentum posuere urna nec. Donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Leo urna molestie at elementum. Ultrices vitae auctor eu augue. Morbi quis commodo odio aenean sed adipiscing diam donec adipiscing. Ut consequat semper viverra nam libero justo laoreet sit amet. Sed pulvinar proin gravida hendrerit lectus a. Tristique magna sit amet purus gravida quis blandit. Et tortor consequat id porta nibh venenatis cras sed. Neque sodales ut etiam sit amet. Eget nulla facilisi etiam dignissim diam quis enim. In ornare quam viverra orci sagittis.",
                         Price = 300000,
                         UploadDate = DateTime.Parse("2023-7-10"),
 
